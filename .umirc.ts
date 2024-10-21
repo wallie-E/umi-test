3deddedc82a800b583bbe0334c9770a9d614e8ff
chenfengqi <chenfengqi@192.168.0.103>
2024-10-21 21:23:32 +0800

    double

--- .umirc.ts
+++ .umirc.ts
@@ -4,9 +4,6 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [
-    { path: '/', component: '@/pages/index' },
-  ],
-  fastRefresh: {}
-  
+  routes: [{ path: '/', component: '@/pages/index' }],
+  fastRefresh: {},
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,26 @@
 import styles from './index.less';
-import { Button }  from 'antd';
+import { Button } from 'antd';
 import { useEffect } from 'react';
 
 export default function IndexPage() {
   useEffect(() => {
-    if (PerformanceObserver.supportedEntryTypes.includes('paint')) {
+    if (PerformanceObserver.supportedEntryTypes.includes("paint")) {
       const observer = new PerformanceObserver((list) => {
         const entries = list.getEntriesByName('first-contentful-paint');
         if (entries.length > 0) {
           const fcpEntry = entries[0];
-          console.log("FCP:", fcpEntry.startTime);
+          console.log('FCP:', fcpEntry.startTime);
           // 你可以将 FCP 数据发送到你的后端或分析工具
         }
       });
-      
+
       observer.observe({ type: 'paint', buffered: true });
     }
-    
-  }, [])
+  }, []);
 
   return (
     <>
       <Button>hhh</Button>
     </>
-  )
+  );
 }
